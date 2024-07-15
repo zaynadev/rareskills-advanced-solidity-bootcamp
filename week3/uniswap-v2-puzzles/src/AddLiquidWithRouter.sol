5b98412336aebe3bff1f4ea58392b3eebf6c6fae
zaynadev <zayna.oufkir@gmail.com>
2024-07-15 21:07:26 +0100

    week3/uniswap-v2-puzzles: AddLiquidWithRouter

--- week3/uniswap-v2-puzzles/src/AddLiquidWithRouter.sol
+++ week3/uniswap-v2-puzzles/src/AddLiquidWithRouter.sol
@@ -1,7 +1,13 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
+import "forge-std/console.sol";
 import "./interfaces/IUniswapV2Pair.sol";
+
+interface IERC20 {
+    function balanceOf(address account) external view returns (uint256);
+    function approve(address spender, uint256 amount) external returns (bool);
+}
 
 contract AddLiquidWithRouter {
     /**
@@ -13,13 +19,80 @@ contract AddLiquidWithRouter {
      *
      */
     address public immutable router;
+    // price feed mainnet address USDC/ETH
+    address public priceFeed = 0x986b5E1e1755e3C2440e960477f25201B0a8bbD4;
+
+    // 1 hour grace period
+    uint256 public constant GRACE_PERIOD_SECONDS = 3600; // 1 hour
+    // heartbeat 24 hours: the price is considered outdated after HEARTBEAT_SECONDS + GRACE_PERIOD_SECONDS
+    uint256 public constant HEARTBEAT_SECONDS = 86400; //  24 hours
+
+    error PriceIsOutdated();
+    error PriceIsNegative();
 
     constructor(address _router) {
         router = _router;
     }
 
+    /**
+     * @notice  . Add liquidity to the pool USDC/ETH using the Uniswapv2 router.
+     * @dev     . The contract has an initial balance of 1000 USDC and 1 ETH.
+     * @param   usdcAddress  . The USDC address.
+     * @param   deadline  . The timestamp after which the transaction will revert.
+     */
     function addLiquidityWithRouter(address usdcAddress, uint256 deadline) public {
-        // your code start here
+        // amount of USDC and ETH in the contract
+        uint256 amountUSDCDesired = IERC20(usdcAddress).balanceOf(address(this));
+        uint256 amountETHDesired = address(this).balance;
+
+        // price of USDC in terms of ETH
+        // 1 USDC =  ? ETH
+        uint256 price = getLatestPrice(priceFeed);
+
+        // amount of ETH optimal to add as liquidity
+        // 1e6 * 1e18 / 1e6 = 1e18
+        uint256 amountETHOptimal = (amountUSDCDesired * price) / 1e6;
+
+        // tolerate 1% price impact
+        uint256 amountUSDCMin = (amountUSDCDesired * 99) / 100;
+        uint256 amountETHMin =
+            amountETHOptimal <= amountETHDesired ? (amountETHOptimal * 99) / 100 : (amountETHDesired * 99) / 100;
+
+        // approve USDC to the router
+        IERC20(usdcAddress).approve(router, amountUSDCDesired);
+
+        // add liquidity to the pool USDC/ETH
+        IUniswapV2Router(router).addLiquidityETH{value: amountETHDesired}(
+            usdcAddress, amountUSDCDesired, amountUSDCMin, amountETHMin, msg.sender, deadline
+        );
+    }
+
+    /**
+     * @notice  . Get the latest price from the Chainlink price feed.
+     * @dev     . Check if the price has been updated within the acceptable time frame.
+     * @return  price  . The latest price.
+     */
+    function getLatestPrice(address _priceFeed) public view returns (uint256) {
+        // get the latest price using chainlink price feed
+        (uint80 roundID, int256 price,, uint256 timestamp, uint80 answeredInRound) =
+            AggregatorV3Interface(_priceFeed).latestRoundData();
+
+        // Check if the price has been updated within the acceptable time frame
+        if (block.timestamp - timestamp > HEARTBEAT_SECONDS + GRACE_PERIOD_SECONDS) {
+            revert PriceIsOutdated();
+        }
+
+        // Check if the round is complete
+        if (answeredInRound < roundID) {
+            revert PriceIsOutdated();
+        }
+
+        // Check if the price is positive
+        if (price <= 0) {
+            revert PriceIsNegative();
+        }
+
+        return uint256(price);
     }
 
     receive() external payable {}
@@ -42,4 +115,11 @@ interface IUniswapV2Router {
         address to,
         uint256 deadline
     ) external payable returns (uint256 amountToken, uint256 amountETH, uint256 liquidity);
+}
+
+interface AggregatorV3Interface {
+    function latestRoundData()
+        external
+        view
+        returns (uint80 roundId, int256 answer, uint256 startedAt, uint256 updatedAt, uint80 answeredInRound);
 }
